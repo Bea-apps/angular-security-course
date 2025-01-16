3d3bf9b577b61ef244ae40711fec13084065f474
Beatriz Melgar Rodriguez <bea.melgar.rod@gmail.com>
2025-01-16 17:01:26 +0100

    8.58 - Retrieve and validate auth0 jwt.

--- server/server.ts
+++ server/server.ts
@@ -5,7 +5,14 @@ import {Application} from "express";
 import * as fs from 'fs';
 import * as https from 'https';
 import {readAllLessons} from "./read-all-lessons.route";
+
+
 const bodyParser = require('body-parser');
+
+// caution: this project work only with jwks-rsa version 1.12.3
+// and express-jwt version 5.3.0.
+const jwksRsa = require('jwks-rsa');
+const jwt = require('express-jwt');
 
 const app: Application = express();
 
@@ -19,7 +26,34 @@ const optionDefinitions = [
 
 const options = commandLineArgs(optionDefinitions);
 
+
+// jwksRsa for retrieving the public key from auth0 website.
+const checkIfAuthenticated = jwt({
+    secret: jwksRsa.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksUri: "https://dev-kx0725jblw8655rw.us.auth0.com/.well-known/jwks.json"
+
+    }),
+    algorithms: ['RS256']
+});
+
+app.use(checkIfAuthenticated);
+
+app.use((err, req, res, next) => {
+    if(err && err.name == "UnauthorizedError") {
+        res.status(err.status).json({message: err.message});
+    } else {
+        // if there is no problem and JWT was validated it =>
+        // we continue.
+        next();
+    }
+
+});
+
+
 // REST API
+// => if readAllLessons go through, it means the application has a valid JWT.
 app.route('/api/lessons')
     .get(readAllLessons);
 
